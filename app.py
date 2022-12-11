91d2629d8ef148033505aa39684ed96266906228
wayneii <mwayneroweii@gmail.com>
2022-12-11 01:27:42 -0500

    another one

--- app.py
+++ app.py
@@ -3,8 +3,8 @@ import streamlit as st
 st.title("Let's see if I can predict if you used LinkedIn")
 
 #Age
-age = st.slider(label="Enter you age", min_value=1, max_value=98,value=7)
-st.write("Your age is: ", age)
+#age = st.slider(label="Enter you age", min_value=1, max_value=98,value=7)
+#st.write("Your age is: ", age)
 
 
 #Gender
