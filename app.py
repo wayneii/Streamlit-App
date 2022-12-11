e5a731fb6c1a2f7967c835247d0c4ef227d0bd4d
wayneii <mwayneroweii@gmail.com>
2022-12-11 00:16:32 -0500

    Update Slider Again

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ st.title("Let's see if I can predict if you used LinkedIn")
 
 #Age
 age = st.slider(label="Enter you age", min_value=1, max_value=98,value=7)
+st.write("Your age is: ", age)
 
 
 #Gender
