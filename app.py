c5cb451ec4072b1f03d80073bd8d6202a07dc4a4
wayneii <mwayneroweii@gmail.com>
2022-12-11 00:52:37 -0500

    Another Update

--- app.py
+++ app.py
@@ -17,23 +17,23 @@ st.write("Your age is: ", age)
 #parent = st.radio("Are you a parent?", ('Yes', 'No'))
 
 #Highest Level of Eduction
-income = st.selectbox("Highest Level of Education", 
-              options = ["Less than high school (Grades 1-8 or no formal schooling)",
-                         "High school graduate (Grade 12 with diploma or GED certificate)",
-                         "High school graduate (Grade 12 with diploma or GED certificate)",
-                         "Some college, no degree (includes some community college)",
-                         "Two-year associate degree from a college or university",
-                         "Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)",
-                         "Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)",
-                         "Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)"])
-#Income
-income = st.selectbox("Income Level", 
-              options = ["Less than $10,000",
-                         "10 to under $20,000",
-                         "20 to under $30,000",
-                         "30 to under $40,000",
-                         "40 to under $50,000",
-                         "50 to under $75,000",
-                         "75 to under $100,000",
-                         "100 to under $150,000",
-                         "$150,000 or more"])
+#income = st.selectbox("Highest Level of Education", 
+#              options = ["Less than high school (Grades 1-8 or no formal schooling)",
+#                         "High school graduate (Grade 12 with diploma or GED certificate)",
+#                         "High school graduate (Grade 12 with diploma or GED certificate)",
+#                         "Some college, no degree (includes some community college)",
+#                         "Two-year associate degree from a college or university",
+#                         "Four-year college or university degree/Bachelor’s degree (e.g., BS, BA, AB)",
+#                         "Some postgraduate or professional schooling, no postgraduate degree (e.g. some graduate school)",
+#                         "Postgraduate or professional degree, including master’s, doctorate, medical or law degree (e.g., MA, MS, PhD, MD, JD)"])
+##Income
+#income = st.selectbox("Income Level", 
+#              options = ["Less than $10,000",
+#                         "10 to under $20,000",
+#                         "20 to under $30,000",
+#                         "30 to under $40,000",
+#                         "40 to under $50,000",
+#                         "50 to under $75,000",
+#                         "75 to under $100,000",
+#                         "100 to under $150,000",
+#                         "$150,000 or more"])
